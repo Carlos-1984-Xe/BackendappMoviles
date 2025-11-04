9dfe4ec0b12377f669d522897f48d403450a9246
Carlos-1984-Xe <carl.munozl@duocuc.cl>
2025-11-04 13:41:29 -0300

    Initial commit

---
+++ src/main/java/com/MiDiVentas/appMoviles/BackendMiDiApplication.java
@@ -0,0 +1,13 @@
+package com.MiDiVentas.appMoviles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMiDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMiDiApplication.class, args);
+	}
+
+}
